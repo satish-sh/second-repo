78da45e9127de878bb7fc01e0791a91f2000ce72
satish-sh <satish-sh@IND-PG0395X4.localdomain>
2024-09-10 11:31:37 +0530

    modified

--- even.py
+++ even.py
@@ -13,4 +13,14 @@ if 10 < a < 40:
             list_A.append(i)
 
 print(list_A)
+<<<<<<< HEAD
 >>>>>>> 8fed38d (even number in between two digits)
+=======
+
+list_B = []
+for i in range(user_input):
+    if i%2 != 0:
+        list_B.append(i)
+
+print(list_B)
+>>>>>>> 498575a (modified)
