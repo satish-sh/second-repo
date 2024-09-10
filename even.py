32541eb364a9d204c4771e490da9507cd910420a
satish-sh <satish-sh@IND-PG0395X4.localdomain>
2024-09-10 11:28:47 +0530

    modified by the user

--- even.py
+++ even.py
@@ -1,5 +1,16 @@
+<<<<<<< HEAD
 user_input = int(input())
 
 for i in range(user_input):
     if i % 2 == 0:
         print(f"{i} is even number")
+=======
+a = int(input())
+list_A  = []
+if 10 < a < 40:
+    for i in range(a):
+        if i % 2 == 0:
+            list_A.append(i)
+
+print(list_A)
+>>>>>>> 8fed38d (even number in between two digits)
